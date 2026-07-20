c9d368e6509a7c70477bf01532790fafaa57de87
kanpianer <pianer@riseup.net>
2026-07-20 06:52:38 -0700

    删除无用的文件和文件夹，不要影响app功能

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ export default defineConfig(() => {
       environment: 'jsdom',
       globals: true,
       setupFiles: ['./src/test-setup.ts'],
+      exclude: ['**/node_modules/**', '**/dist/**', '**/.alma-snapshots/**', '**/.git/**'],
     },
   };
 });
